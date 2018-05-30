30aecc1c6cf621fc62fa2f13d998d08e5783a49c
Jean Catanho <jean.catanho@corp.globo.com>
2018-05-30 17:49:58 -0300

    Add game state logic

--- lib/Manager.lua
+++ lib/Manager.lua
@@ -2,26 +2,47 @@ local RedisClient = require('lib.RedisClient')
 
 Manager = {}
 
+GameState = {Menu = 0, Highscore = 1, Game = 2, End = 3}
+
 -- Manager constructor
 function Manager.new()
 	local self = {}
 
-	local redisClient = RedisClient.new()
+	local redisClient = nil
+	local state = nil
 
 	function self.load()
-
+		redisClient = RedisClient.new()
+		state = GameState.Menu
 	end
 
 	function self.update(dt)
-
+		if love.keyboard.isDown('m') then
+			state = GameState.Menu
+		elseif love.keyboard.isDown('h') then
+			state = GameState.Highscore
+		elseif love.keyboard.isDown('g') then
+			state = GameState.Game
+		elseif love.keyboard.isDown('e') then
+			state = GameState.End
+		end
 	end
 
 	function self.draw()
 		local width, height, _ = love.window.getMode()
 
-		if redisClient.ping() then
-    		love.graphics.print("Hello, world!", width/2, height/2)
+		if state == GameState.Menu then
+			love.graphics.print("Menu", width/2, height/2)
+		elseif state == GameState.Highscore then
+			love.graphics.print("Highscore", width/2, height/2)
+		elseif state == GameState.Game then
+			love.graphics.print("Game", width/2, height/2)
+		elseif state == GameState.End then
+			love.graphics.print("End", width/2, height/2)
+		else
+			love.graphics.print("Error! Unknown state!", width/2, height/2)
 		end
+		
 	end
 
 	return self

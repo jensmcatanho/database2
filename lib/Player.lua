3ea06f0829636e2a0fbaab3fef71926b50861be5
Jean Catanho <jensmcatanho@gmail.com>
2018-06-04 02:59:45 -0300

    Add player movement

--- lib/Player.lua
+++ lib/Player.lua
@@ -24,8 +24,16 @@ function Player.new()
 
     local state = nil
 
-    self.x = 400
-    self.y = 300
+    self.position = {
+        x = 400 - sprites.idle:getWidth() * 0.5,
+        y = 300 - sprites.idle:getHeight() * 0.5
+    }
+    
+    local yaw = math.rad(270)
+    self.lookAt = {
+        x = math.sin(yaw),
+        y = math.cos(yaw)
+    }
 
 	function self.load()
         state = PlayerState.Idle
@@ -36,44 +44,97 @@ function Player.new()
 
         if (love.keyboard.isDown('a') or love.keyboard.isDown('left')) and (love.keyboard.isDown('w') or love.keyboard.isDown('up')) then
             state = PlayerState.AcceleratingHoveringLeft
+            yaw = yaw + math.rad(2.0)
+            self.lookAt.x, self.lookAt.y = math.sin(yaw), -math.cos(yaw)
+            self.position.x, self.position.y = self.position.x + self.lookAt.x * 3, self.position.y + self.lookAt.y * 3
 
         elseif (love.keyboard.isDown('d') or love.keyboard.isDown('right')) and (love.keyboard.isDown('w') or love.keyboard.isDown('up')) then
             state = PlayerState.AcceleratingHoveringRight
+            yaw = yaw - math.rad(2.0)
+            self.lookAt.x, self.lookAt.y = math.sin(yaw), -math.cos(yaw)
+            self.position.x, self.position.y = self.position.x + self.lookAt.x * 3, self.position.y + self.lookAt.y * 3
 
         elseif love.keyboard.isDown('a') or love.keyboard.isDown('left') then
             state = PlayerState.HoveringLeft
+            yaw = yaw + math.rad(2.0)
+            self.lookAt.x, self.lookAt.y = math.sin(yaw), -math.cos(yaw)
 
         elseif love.keyboard.isDown('d') or love.keyboard.isDown('right') then
             state = PlayerState.HoveringRight
+            yaw = yaw - math.rad(2.0)
+            self.lookAt.x, self.lookAt.y = math.sin(yaw), -math.cos(yaw)
 
         elseif love.keyboard.isDown('w') or love.keyboard.isDown('up') then
             state = PlayerState.Accelerating
+            self.position.x, self.position.y = self.position.x + self.lookAt.x * 3, self.position.y + self.lookAt.y * 3
 
+        end
+
+		local width, height, _ = love.window.getMode()
+        if self.position.x >= width then
+            self.position.x = sprites.idle:getWidth()
+        elseif self.position.x <= 0 then
+            self.position.x = width - sprites.idle:getWidth()
+        end
+
+        if self.position.y >= height then
+            self.position.y = sprites.idle:getHeight()
+        elseif self.position.y <= 0 then
+            self.position.y = height - sprites.idle:getHeight()
         end
 
     end
-    
+
     function self.draw()
         if state == PlayerState.Idle then
-            love.graphics.draw(sprites.idle, self.x, self.y)
+            love.graphics.draw(
+                sprites.idle,
+                self.position.x - sprites.idle:getWidth() * 0.5,
+                self.position.y - sprites.idle:getHeight() * 0.5,
+                yaw
+            )
             
         elseif state == PlayerState.Accelerating then
-            love.graphics.draw(sprites.accelerating, self.x, self.y)
+            love.graphics.draw(
+                sprites.accelerating,
+                self.position.x - sprites.accelerating:getWidth() * 0.5,
+                self.position.y - sprites.accelerating:getHeight() * 0.5,
+                yaw
+        )
             
         elseif state == PlayerState.HoveringLeft then
-            love.graphics.draw(sprites.hovering_left, self.x, self.y)
+            love.graphics.draw(
+                sprites.hovering_left,
+                self.position.x - sprites.hovering_left:getWidth() * 0.5,
+                self.position.y - sprites.hovering_left:getHeight() * 0.5,
+                yaw
+            )
         
         elseif state == PlayerState.HoveringRight then
-            love.graphics.draw(sprites.hovering_right, self.x, self.y)
+            love.graphics.draw(
+                sprites.hovering_right,
+                self.position.x - sprites.hovering_right:getWidth() * 0.5,
+                self.position.y - sprites.hovering_right:getHeight() * 0.5,
+                yaw
+            )
 
         elseif state == PlayerState.AcceleratingHoveringLeft then
-            love.graphics.draw(sprites.accelerating_hovering_left, self.x, self.y)
+            love.graphics.draw(
+                sprites.accelerating_hovering_left,
+                self.position.x - sprites.accelerating_hovering_left:getWidth() * 0.5,
+                self.position.y - sprites.accelerating_hovering_left:getHeight() * 0.5,
+                yaw
+            )
 
         elseif state == PlayerState.AcceleratingHoveringRight then
-            love.graphics.draw(sprites.accelerating_hovering_right, self.x, self.y)
+            love.graphics.draw(
+                sprites.accelerating_hovering_right,
+                self.position.x - sprites.accelerating_hovering_right:getWidth() * 0.5,
+                self.position.y - sprites.accelerating_hovering_right:getHeight() * 0.5,
+                yaw
+            )
 
         end
-    
     end
 
 	return self
